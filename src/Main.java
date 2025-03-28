040f8b17766898e6d4ba22f11457d49643296f4a
Abayev <240918@astanait.edu.kz>
2025-03-28 19:43:25 +0500

    First Task is completed

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,39 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // enter the length of array
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int task_1 = findMinimum(arr);
+        System.out.println(task_1);
+
 
     }
 
-    public static int fibonacci(int n) {
-        if (n == 0 || n == 1) return 0;
 
+    /**
+     * this method find the minimum element in the array
+     * @param arr accept array of integer
+     * @return the minimum in the given array
+     */
+    public static int findMinimum(int[] arr) {
+        // initialize our minimum
+        int min = arr[0];
 
+        for (int num : arr) {
+            if (num < min) {
+                min = num;
+            }
+        }
+        // return minimum element
+        return min;
     }
 }
